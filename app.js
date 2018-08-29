d19099770b6155211863519d7d7ed216f8d44d8a
Anthony Forte <ray2604@gmail.com>
2018-08-29 14:19:52 -0500

    Created assignment tutorials

---
+++ app.js
@@ -0,0 +1,9 @@
+//This first output is for the 1,2,3 Code tutorial
+console.log("Anthony");
+console.log(30 + 3);
+
+console.log(""); //Adding a space
+
+//Second output is for the Play with Variables tutorial
+const d = 32;
+console.log("The temp of " + d + "C° in F° is: " + (d * 1.8 + 32));
